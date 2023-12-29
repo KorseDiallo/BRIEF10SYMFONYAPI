f46dc75c23c40c7fd6be5968df773f1a8d17079e
KorseDiallo <abdoulayekorsediallo@gmail.com>
2023-12-29 15:58:21 +0000

    fonctionnaliter modifier formation

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Serializer\SerializerInterface;
 
 class FormationController extends AbstractController
@@ -32,6 +33,21 @@ class FormationController extends AbstractController
            return new JsonResponse(['message' => 'La Formation a été supprimée avec Succès'], Response::HTTP_OK);
         }else{
             return new JsonResponse(['message' => 'Formation non trouvée'], Response::HTTP_OK);
+        }
+   }
+
+   #[Route('/api/modifierFormation/{id}', name: 'modifier_formation')]
+   public function modifierFormation(int $id, Request $request,SerializerInterface $serializer,FormationRepository $formationRepository,EntityManagerInterface $em){
+        $modifFormation= $formationRepository->find($id);
+        if($modifFormation){
+            $jsonModifFormation= $serializer->deserialize($request->getContent(),Formation::class,'json',[AbstractNormalizer::OBJECT_TO_POPULATE =>$modifFormation]);
+
+            $em->persist($jsonModifFormation);
+            $em->flush();
+
+            return new JsonResponse(['message' => 'La Formation a été modifiée avec Succès'], Response::HTTP_OK);
+        }else{
+            return new JsonResponse(['message' => 'La Formation que vous voulez modifier n\'a pas été trouvé'], Response::HTTP_OK);
         }
    }
 }
