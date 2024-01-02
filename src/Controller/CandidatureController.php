568ddfb34f0fd81975d44630024789609e944015
KorseDiallo <abdoulayekorsediallo@gmail.com>
2024-01-02 12:24:22 +0000

    fonctionnalite candidater

--- src/Controller/CandidatureController.php
+++ src/Controller/CandidatureController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Candidature;
+use App\Repository\FormationRepository;
+use App\Repository\StatutRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,16 +16,34 @@ use Symfony\Component\Serializer\SerializerInterface;
 
 class CandidatureController extends AbstractController
 {
-    #[Route('/api/candidater', name: 'app_candidater')]
-    public function creerFormation(Request $request,SerializerInterface $serializer,EntityManagerInterface $em): JsonResponse
-    {
-        // dd($request->getContent());
-        $candidature=$serializer->deserialize($request->getContent(),Candidature::class,'json');
-        //dd($candidature);
+    #[Route('/api/candidater/{formationId}/{statutId}', name: 'app_candidater')]
+    public function creerFormation(EntityManagerInterface $em,Security $security,int $formationId,int $statutId,FormationRepository $formationRepository,StatutRepository $statutRepository ): JsonResponse {
+        
+        $user = $this->getUser();
+
+        if (!$user) {
+            return new JsonResponse(['message' => 'Utilisateur non connecté'], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $formation = $formationRepository->find($formationId);
+        
+        $statut = $statutRepository->find($statutId);
+
+        if (!$formation || !$statut) {
+            return new JsonResponse(['message' => 'Formation ou statut non trouvé'], Response::HTTP_NOT_FOUND);
+        }
+
+        $candidature = new Candidature();
+        
+        $candidature->setUsers($user)
+                    ->setFormations($formation)
+                    ->setStatut($statut);
+
         $em->persist($candidature);
         $em->flush();
-        return new JsonResponse(['message' => 'Candidature enregistrer avec Succès'], Response::HTTP_CREATED);
+
+        return new JsonResponse(['message' => 'Candidature enregistrée avec succès'], Response::HTTP_CREATED);
     }
 
- 
+    
 }
